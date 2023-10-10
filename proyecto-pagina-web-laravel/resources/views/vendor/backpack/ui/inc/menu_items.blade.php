3812b46bfe9491c6fbf90c08023b3e72f3f8b8f4
joaquincernik <joaquincernik@gmail.com>
2023-10-10 17:57:34 -0300

    menu items

--- proyecto-pagina-web-laravel/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ proyecto-pagina-web-laravel/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,7 +1,7 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
-<x-backpack::menu-item title="Teléfonos útiles" icon="la la-phone" :link="backpack_url('phone')" />
+<x-backpack::menu-item title="Teléfonos útiles" icon="la la-tv" :link="backpack_url('phone')" />
 <x-backpack::menu-item title="Info Cooperativa" icon="la la-tv" :link="backpack_url('infocoop')" />
 <x-backpack::menu-item title="Info Municipal" icon="la la-tv" :link="backpack_url('infoMuni')" />
 <x-backpack::menu-item title="Info Remises" icon="la la-tv" :link="backpack_url('infoTaxi')" />
